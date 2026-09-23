24d29a6bdd17299c2a9d51629c4f9365c95581bb
undus5 <mail@undus.net>
2026-09-23 17:56:23 +0800

    update rpmfusion repo mirror url

--- uboot/5-rpmfusion.sh
+++ uboot/5-rpmfusion.sh
@@ -14,6 +14,12 @@ NONFREE_URL=${BASE_URL}/${NONFREE_PATH}
 dnf install -y $FREE_URL
 dnf install -y $NONFREE_URL
 
+sed -e 's!^metalink=!#metalink=!g' \
+         -e 's!^mirrorlist=!#mirrorlist=!g' \
+         -e 's!^#baseurl=!baseurl=!g' \
+         -e 's!https\?://download1\.rpmfusion\.org/!https://mirrors.cernet.edu.cn/rpmfusion/!g' \
+         -i.old /etc/yum.repos.d/rpmfusion*.repo
+
 dnf swap -y ffmpeg-free ffmpeg --allowerasing
 dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld --allowerasing
 dnf install -y mesa-va-drivers-freeworld intel-media-driver
