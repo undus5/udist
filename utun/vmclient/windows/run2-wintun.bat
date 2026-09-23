a6547e681b66aedbc0f1279478c759ad23b4a8aa
undus5 <mail@undus.net>
2026-09-23 23:19:05 +0800

    update wintun script

--- utun/vmclient/windows/run2-wintun.bat
+++ utun/vmclient/windows/run2-wintun.bat
@@ -1,7 +1,8 @@
-REM wintun batch
+REM documentation
+REM https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/route_ws2008
+REM https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/cc731521(v=ws.10)
 
 route delete 0.0.0.0
 netsh interface ip set address wintun static 172.50.60.70 255.255.255.0 none
 timeout /nobreak /t 5
 route add 0.0.0.0 mask 0.0.0.0 172.50.60.70
-
